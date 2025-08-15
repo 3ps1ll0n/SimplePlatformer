af8c60c4dd9c8595f918fc91d13ed5f300be6e83
BobNe0 <lessard.neo@gmail.com>
2025-08-15 19:59:02 -0400

    Acceleration and deceleration (run and jump)
    some more satisfacting control

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,26 +1,36 @@
 extends CharacterBody2D
+#  List des variables
 
 @export var SPEED = 300.0
+@export_range(0,1) var acceleration = 0.1
+@export_range(0,1) var deceleration = 0.1
+
 @export var JUMP_VELOCITY = -400.0
+@export_range(0,1) var decelerate_on_jump_release = 0.5
+
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-func _physics_process(delta: float) -> void:
+func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
+
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
+	if not Input.is_action_pressed("Jump") and velocity.y < 0:
+		velocity.y *=decelerate_on_jump_release
+	
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("Left", "Right")
+	var direction := Input.get_axis("Move Left", "Move Right")
+	
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = move_toward(velocity.x, direction * SPEED, SPEED * acceleration)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, SPEED * deceleration)
 	
 	#========================================== Animation Section ==========================================
 	

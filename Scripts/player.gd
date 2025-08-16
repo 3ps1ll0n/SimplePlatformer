a29b3bcae238b034ab32a00808c41ad7e9ed327a
BobNe0 <lessard.neo@gmail.com>
2025-08-16 01:28:59 -0400

    Dash Bidirectionel
    -ajout de quelque platform
    -meilleur placement de caméra
    -Update speed
    -Un dash a gauche ou a droite que tu peux faire une fois maximum en l'aire avant de devoir toucher le sol.
    Le dash se stop sur les murs et suit une courbe de vitesse progressive

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 #  List des variables
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-@export var speed = 300.0
+@export var speed = 200.0
 @export_range(0,1) var acceleration = 0.1
 @export_range(0,1) var deceleration = 0.1
 
@@ -10,16 +10,19 @@ extends CharacterBody2D
 @export_range(0,1) var decelerate_on_jump_release = 0.5
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-@export var dash_speed = 300
+@export var dash_speed = 500
 @export var dash_max_distance = 90
-@export var dash_curve = "res://Scenes/Player.tscn::Curve_52ee3"
-@export var dash_cooldown = 0.5
+@export var dash_curve : Curve
 
+var able_to_dash = true
 var is_dashing = false
 var dash_start_position = 0
 var dash_direction = 0
-var dash_timer = 0
 
+var last_direction = 1
+
+func _ready():
+	add_to_group("player")
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -35,17 +38,26 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	var direction := Input.get_axis("Move Left", "Move Right")
 	
+	if direction != 0 :
+		last_direction = direction
+	
 	if direction:
 		velocity.x = move_toward(velocity.x, direction * speed, speed * acceleration)
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed * deceleration)
 	
 	# Dash activation
-	if Input.is_action_just_pressed("Dash") and direction and not is_dashing and dash_timer <= 0:
+	if Input.is_action_just_pressed("Dash") and not is_dashing and able_to_dash:
 		is_dashing = true
+		able_to_dash = false
 		dash_start_position = position.x
-		dash_direction = direction
-		dash_timer = dash_cooldown
+		# use current input direction, otherwise fallback to last direction
+		if direction != 0:
+			dash_direction = direction 
+		else:
+			dash_direction = last_direction 
+	if is_on_floor():
+		able_to_dash = true
 	
 	# Performe actual dash.
 	if is_dashing:
@@ -55,12 +67,6 @@ func _physics_process(delta):
 		else:
 			velocity.x = dash_direction * dash_speed * dash_curve.sample(current_distance/dash_max_distance)
 			velocity.y = 0
-	
-	# Reduce the dash timer.
-	if dash_timer > 0:
-		dash_timer -= delta
-	
-	
 	
 	
 	
@@ -75,6 +81,7 @@ func _physics_process(delta):
 		animated_sprite.flip_h = false
 	elif direction < 0:
 		animated_sprite.flip_h = true
+	
 	
 	if not is_on_floor():
 		if velocity.y < 0:

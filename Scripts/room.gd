e7f36d340665378b6993541b6283911c7aca7ff8
3ps1ll0n <67374616+3ps1ll0n@users.noreply.github.com>
2025-08-15 21:42:28 -0400

    Camera Transition

---
+++ Scripts/room.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		var cam = get_viewport().get_camera_2d()
+		var rect : CollisionShape2D = get_child(0) 
+		cam.set_room_limits(collision_shape_to_rect2(rect))
+
+func collision_shape_to_rect2(collision_shape: CollisionShape2D) -> Rect2:
+	if collision_shape.shape is RectangleShape2D:
+		var rect_shape: RectangleShape2D = collision_shape.shape
+
+		# Taille du rectangle (shape.extents = moitié de la taille)
+		var size = rect_shape.extents * 2
+
+		# Position dans la scène (on combine le transform du node)
+		var pos = collision_shape.global_position - rect_shape.extents
+
+		return Rect2(pos, size)
+	else:
+		push_warning("CollisionShape2D n'est pas un RectangleShape2D")
+		return Rect2()

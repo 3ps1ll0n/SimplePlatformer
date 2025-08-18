94956e4a24ad3499b104c2384165dbd45c8af8cf
BobNe0 <lessard.neo@gmail.com>
2025-08-18 19:59:55 -0400

    Better dash

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -13,12 +13,14 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @export var dash_speed = 500
 @export var dash_curve : Curve
 
+var able_to_jump = true
 var able_to_dash = true
 var is_dashing = false
+var is_jumping = false
 var dash_start_position : Vector2
 var dash_direction : Vector2 = Vector2.ZERO
-
 var last_direction : Vector2 = Vector2.RIGHT
+
 # For camera
 func _ready():
 	add_to_group("player")
@@ -26,13 +28,16 @@ func _ready():
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
+		able_to_jump = false
 		velocity.y += gravity * delta
 	# Handle jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and able_to_jump and not is_dashing:
 		velocity.y = jump_velocity
-	
-	if not Input.is_action_pressed("Jump") and velocity.y < 0:
+		is_dashing = false
+		is_jumping = true
+	if not Input.is_action_pressed("Jump") and velocity.y < 0 and not is_dashing:
 		velocity.y *=decelerate_on_jump_release
+	
 	
 	# Get input direction as Vector2
 	var input_direction = Vector2(
@@ -54,6 +59,7 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("Dash") and not is_dashing and able_to_dash:
 		is_dashing = true
 		able_to_dash = false
+		is_jumping = false
 		dash_start_position = position
 		if input_direction != Vector2.ZERO:
 			dash_direction = input_direction
@@ -64,7 +70,7 @@ func _physics_process(delta):
 		
 	if not is_dashing and is_on_floor():
 		able_to_dash = true
-	
+		able_to_jump = true
 	# Performe actual dash.
 	if is_dashing:
 

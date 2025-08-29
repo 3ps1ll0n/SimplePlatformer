90066794be6fd66e275c777b6e97dce328b78ad8
3ps1ll0n <67374616+3ps1ll0n@users.noreply.github.com>
2025-08-29 17:04:58 -0400

    You can attack

---
+++ Scripts/attack_hit_box.gd
@@ -0,0 +1,21 @@
+extends Area2D
+@onready var collision_shape = $CollisionShape2D
+
+func _ready() -> void:
+	connect("body_entered", Callable(self, "_on_body_entered"))
+
+func _on_body_entered(body: Node) -> void:
+	if body.is_in_group("enemies"):   # Assure-toi que tes ennemis sont dans ce groupe
+		#body.take_damage(1)           # Appelle une fonction de dégâts
+		pass
+
+
+func set_shape(shape: Shape2D, size: Vector2) -> void:
+	collision_shape.shape = shape
+	if shape is RectangleShape2D:
+		shape.size = size
+	elif shape is CapsuleShape2D:
+		shape.radius = size.x
+		shape.height = size.y
+	elif shape is CircleShape2D:
+		shape.radius = size.x

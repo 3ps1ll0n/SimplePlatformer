dd09637052bb215103cacb3f867717de48467c56
3ps1ll0n <67374616+3ps1ll0n@users.noreply.github.com>
2025-09-27 20:57:04 -0400

    Big fat update

--- Scripts/attack_hit_box.gd
+++ Scripts/attack_hit_box.gd
@@ -14,15 +14,16 @@ func _ready() -> void:
 	connect("body_entered", Callable(self, "_on_body_entered"))
 
 func _on_body_entered(body: Node) -> void:
-	if body.is_in_group("enemies"):   # Assure-toi que tes ennemis sont dans ce groupe
+	if body.is_in_group("enemies") and team != TEAM.ENEMY:   # Assure-toi que tes ennemis sont dans ce groupe
 		body.take_damage(damage)
 		var player := get_parent().get_parent()
 		if player is CharacterBody2D:
 			body.take_knockback(player.position)
-	elif body.is_in_group("player"):
+	elif body.is_in_group("player") and team != TEAM.PLAYER:
 		body.take_damage(damage)
-		var ennemi := get_parent().get_parent()
+		var ennemi := get_parent()
 		body.take_knockback(ennemi.position)
+		body.trigger_invincibility()
 
 
 func set_shape(shape: Shape2D, size: Vector2) -> void:
@@ -35,16 +36,8 @@ func set_shape(shape: Shape2D, size: Vector2) -> void:
 	elif shape is CircleShape2D:
 		shape.radius = size.x
 
-func set_properties(damage: int, team: TEAM, shape: Shape2D, size: Vector2):
-	self.team = team
-	self.damage = damage
+func set_properties(damage_value: int, hb_team: TEAM, shape: Shape2D, size: Vector2):
+	team = hb_team
+	damage = damage_value
 	
-	collision_shape.shape = shape
-	if shape is RectangleShape2D:
-		shape.size = size
-	elif shape is CapsuleShape2D:
-		shape.radius = size.x
-		shape.height = size.y
-	elif shape is CircleShape2D:
-		shape.radius = size.x
-	pass
+	set_shape(shape, size)

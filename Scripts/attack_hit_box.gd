20d794712d381140a003011bcee35c681961f5ac
Lucas Siwek <67374616+3ps1ll0n@users.noreply.github.com>
2025-10-01 21:48:29 -0400

    Revert "Hud"

--- Scripts/attack_hit_box.gd
+++ Scripts/attack_hit_box.gd
@@ -1,6 +1,5 @@
 extends Area2D
-
-var player : CharacterBody2D
+@onready var collision_shape = $CollisionShape2D
 
 enum TEAM {
 	UNDIFINED,
@@ -10,22 +9,16 @@ enum TEAM {
 
 var team := TEAM.UNDIFINED
 var damage := 0
-var knock_back_multiplier = 1.0
-var collision_shape
-var from_entity : CharacterBody2D = null
 
 func _ready() -> void:
 	connect("body_entered", Callable(self, "_on_body_entered"))
-	collision_shape = get_child(0)
-	player = get_tree().get_first_node_in_group("player")
 
 func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("enemies") and team != TEAM.ENEMY:   # Assure-toi que tes ennemis sont dans ce groupe
-		body.take_knockback(player.position, knock_back_multiplier)
-		if player.get_attack_direction() == Vector2.DOWN and not body.is_dead():
-			player.velocity.y = -250
 		body.take_damage(damage)
-		
+		var player := get_parent().get_parent()
+		if player is CharacterBody2D:
+			body.take_knockback(player.position)
 	elif body.is_in_group("player") and team != TEAM.PLAYER:
 		body.take_damage(damage)
 		var ennemi := get_parent()
@@ -48,23 +41,3 @@ func set_properties(damage_value: int, hb_team: TEAM, shape: Shape2D, size: Vect
 	damage = damage_value
 	
 	set_shape(shape, size)
-
-func set_new_hit_box(hb : CollisionShape2D):
-	var new_shape = hb.shape.duplicate() # copie indépendante
-	var my_shape : CollisionShape2D = get_child(0) # celui à remplacer
-	my_shape.shape = new_shape
-	
-func set_new_hit_box_polygone(hb : CollisionPolygon2D):
-	var new_poly = CollisionPolygon2D.new()
-	new_poly.polygon = hb.polygon.duplicate()  # makes a copy of the array
-	remove_child(get_child(0))
-	add_child(new_poly)
-
-func set_knockback(kb_multiplier : float):
-	knock_back_multiplier = kb_multiplier
-
-func set_damage(damage_value: int):
-	damage = damage_value
-	
-func set_team(hb_team: TEAM):
-	team = hb_team
--- Scripts/enemy_class.gd
+++ Scripts/enemy_class.gd
@@ -2,8 +2,8 @@ extends CharacterBody2D
 
 @onready var flash_animation: AnimationPlayer = $AnimatedSprite2D/FlashAnimation
 
-var max_health := 100
-var knockback_strength: float = 200.0
+@export var max_health := 100
+@export var knockback_strength: float = 200.0
 
 var current_health := max_health
 var knockback_velocity: Vector2 = Vector2.ZERO
@@ -21,16 +21,16 @@ func take_damage(damage: int) -> void:
 	if current_health <= 0:
 		die()
 
-func take_knockback(from_position : Vector2, kb_multiplier := 1.0):
+func take_knockback(from_position : Vector2):
 	var direction = sign(global_position.x - from_position.x) # +1 if hit from left, -1 if hit from right
-	knockback_velocity.x = direction * knockback_strength * kb_multiplier
+	knockback_velocity.x = direction * knockback_strength
 	# optionally add some Y if you want a pop-up effect
 	knockback_velocity.y = -100
 
 func die() -> void:
 	dead = true
 	set_collision_layer_value(1, false)
-	set_collision_layer_value(3, true)
+	set_collision_layer_value(2, true)
 	#queue_free()
 
 func _physics_process(delta: float) -> void:
